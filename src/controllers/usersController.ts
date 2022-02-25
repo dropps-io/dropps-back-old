e48a4c8a2b6cc951b0c5a8b087c90b3b28e369d4
Samuel VIDEAU <samuel.videau@efrei.net>
2022-02-25 12:14:30 +0100

    feat: not-found return for GET user

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -13,11 +13,26 @@ export async function usersController (fastify: FastifyInstance) {
           const {address} = request.params as { address: string };
           if (!isAddress(address)) return reply.code(400).send('Invalid address');
           const user = await queryUser(address);
-          await reply.code(200).send(user);
+          if (!user) return reply.code(404).send('User not found');
+          return  reply.code(200).send(user);
         } catch (e: any) {
           return reply.code(500).send('Internal Error');
         }
       }
     });
-
+  //
+  // fastify.route({
+  //   method: 'POST',
+  //   url: '/:address',
+  //   handler: async (request, reply) => {
+  //     try {
+  //       const {address} = request.params as { address: string };
+  //       if (!isAddress(address)) return reply.code(400).send('Invalid address');
+  //       const user = await queryUser(address);
+  //       await reply.code(200).send(user);
+  //     } catch (e: any) {
+  //       return reply.code(500).send('Internal Error');
+  //     }
+  //   }
+  // });
 }
--- src/db/users.ts
+++ src/db/users.ts
@@ -5,8 +5,7 @@ export async function queryUser(address: string): Promise<User> {
   return new Promise((resolve, reject) => {
     DB.query("SELECT * FROM users WHERE address = '" + address +"';", (err, res) => {
       if (err) reject(err);
-      if (res[0]) resolve(res[0] as User);
-      reject(new Error('No user found with this address'));
+      resolve(res[0] as User);
     });
   });
 }
