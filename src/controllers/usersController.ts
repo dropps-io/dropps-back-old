89793e8738bae31c43784bbc9e74f5223c35a21f
Samuel VIDEAU <samuel.videau@efrei.net>
2022-02-25 13:26:19 +0100

    feat: insert user route

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,9 +1,30 @@
 import { FastifyInstance } from 'fastify';
 import {isAddress} from "../services/utils/validators";
-import {queryUser} from "../db/users";
+import {insertUser, queryUser} from "../db/users";
 
+import * as userSchema from '../models/json/user.json';
+import {User} from "../models/types/user";
 
 export async function usersController (fastify: FastifyInstance) {
+
+    fastify.route({
+      method: 'POST',
+      url: '/',
+      schema: {
+        body: userSchema,
+        response: {200: userSchema}
+      },
+      handler: async (request, reply) => {
+        try {
+          const user = request.body as User;
+          if (await queryUser(user.address)) return reply.code(400).send('User already exists');
+          await insertUser(user.address, user.selectedProfile);
+          return reply.code(200).send(user);
+        } catch (e: any) {
+          return reply.code(500).send('Internal Error');
+        }
+      }
+    });
 
     fastify.route({
       method: 'GET',
@@ -20,19 +41,5 @@ export async function usersController (fastify: FastifyInstance) {
         }
       }
     });
-  //
-  // fastify.route({
-  //   method: 'POST',
-  //   url: '/:address',
-  //   handler: async (request, reply) => {
-  //     try {
-  //       const {address} = request.params as { address: string };
-  //       if (!isAddress(address)) return reply.code(400).send('Invalid address');
-  //       const user = await queryUser(address);
-  //       await reply.code(200).send(user);
-  //     } catch (e: any) {
-  //       return reply.code(500).send('Internal Error');
-  //     }
-  //   }
-  // });
+
 }
--- src/db/users.ts
+++ src/db/users.ts
@@ -3,9 +3,22 @@ import {DB} from "./mysql";
 
 export async function queryUser(address: string): Promise<User> {
   return new Promise((resolve, reject) => {
+
     DB.query("SELECT * FROM users WHERE address = '" + address +"';", (err, res) => {
       if (err) reject(err);
       resolve(res[0] as User);
     });
+
+  });
+}
+
+export async function insertUser(address: string, selectedProfile: string): Promise<User> {
+  return new Promise((resolve, reject) => {
+
+    DB.query("INSERT INTO users VALUES ('" + address +"', '" + selectedProfile + "');", (err, res) => {
+      if (err) reject(err);
+      resolve(res[0] as User);
+    });
+
   });
 }
