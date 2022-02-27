c862f1cef15cfe8178ee47972b3a2362d3c45c4a
Samuel VIDEAU <samuel.videau@efrei.net>
2022-02-27 20:32:31 +0100

    chore: test, dev, and prod environments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import path from "path";
 import { config } from 'dotenv';
+
+export const NODE_ENV = (process.env.NODE_ENV || 'development') as 'test' | 'development' | 'production'
+
+
+if (NODE_ENV === 'test') config({ path: path.resolve(process.cwd(), '.env.test') });
+if (NODE_ENV === 'production') config({ path: path.resolve(process.cwd(), '.env.prod') });
+
 config();
 
 import { fastify } from './lib/fastify';
