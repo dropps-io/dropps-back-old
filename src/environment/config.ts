604bfc5eb93ffb9f5dde2cfcf8af0597d2be5b26
Samuel VIDEAU <samuel.videau@efrei.net>
2022-03-04 16:45:44 +0100

    refactor: JWT_VALIDITY_TIME type is number

--- src/environment/config.ts
+++ src/environment/config.ts
@@ -1,5 +1,5 @@
 import config from 'config';
 
 export const ENV = config.get('env');
-export const JWT_VALIDITY_TIME = config.get('jwt_validity_time');
+export const JWT_VALIDITY_TIME: number = config.get('jwt_validity_time');
 export const LOGGER: boolean = config.get('logger');
