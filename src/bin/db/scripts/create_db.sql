e3380ac7f0d38196308a1aa6019e42c1be076022
Samuel VIDEAU <samuel.videau@efrei.net>
2022-06-01 13:51:59 +0100

    fix: double quotes for uppercase in postgres column names

--- src/bin/db/scripts/create_db.sql
+++ src/bin/db/scripts/create_db.sql
@@ -5,20 +5,20 @@ CREATE DATABASE dropps;
 USE dropps;
 
 CREATE TABLE users (
-  address char(42) NOT NULL,
-  selectedProfile char(42) NOT NULL,
-  PRIMARY KEY (address),
-  UNIQUE (address)
+  "address" char(42) NOT NULL,
+  "selectedProfile" char(42) NOT NULL,
+  PRIMARY KEY ("address"),
+  UNIQUE ("address")
 );
 
 CREATE TABLE user_profile_relations (
-  profileAddress char(42) NOT NULL,
-  userAddress char(42) NOT NULL,
-  archived boolean DEFAULT false,
-  FOREIGN KEY (userAddress) REFERENCES users (address)
+  "profileAddress" char(42) NOT NULL,
+  "userAddress" char(42) NOT NULL,
+  "archived" boolean DEFAULT false,
+  FOREIGN KEY ("userAddress") REFERENCES users ("address")
 );
 
 CREATE TABLE nonces (
-  userAddress char(42) NOT NULL,
-  nonce CHAR(6) NOT NULL,
-  UNIQUE (userAddress));
+  "userAddress" char(42) NOT NULL,
+  "nonce" CHAR(6) NOT NULL,
+  UNIQUE ("userAddress"));
