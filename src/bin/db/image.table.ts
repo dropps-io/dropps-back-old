f173bd27006663e65687280d5a3a712dd78e6d28
Samuel_V <samuel@dropps.io>
2022-07-30 15:53:14 +0100

    test: image tests

--- src/bin/db/image.table.ts
+++ src/bin/db/image.table.ts
@@ -7,12 +7,17 @@ export async function queryImages(address: string): Promise<Image[]> {
 	return res.rows as Image[];
 }
 
-export async function insertImage(address: string, url: string, width: string, height: string, type: ImageType): Promise<Image> {
+export async function queryImagesByType(address: string, type: ImageType): Promise<Image[]> {
+	const res = await executeQuery('SELECT * FROM "image" WHERE "address" = $1 AND "type" = $2', [address, type]);
+	return res.rows as Image[];
+}
+
+export async function insertImage(address: string, url: string, width: number, height: number, type: ImageType): Promise<Image> {
 	const res = await executeQuery('INSERT INTO "image" VALUES ($1, $2, $3, $4, $5)', [address, url, width, height, type]);
 	return res.rows[0] as Image;
 }
 
-export async function updateImage(address: string, url: string, width: string, height: string, type: ImageType): Promise<void> {
-	const res = await executeQuery('UPDATE "image" SET "url" = $2, "width" = $3, "height" = $4, "type" = $5 WHERE "address" = $1', [address, url, width, height, type]);
+export async function deleteImage(address: string, url: string): Promise<void> {
+	const res = await executeQuery('DELETE FROM "image" WHERE "address" = $1 AND "url" = $2', [address, url]);
 	if (res.rowCount === 0) throw ERROR_NOT_FOUND;
 }
---
+++ src/test/unit/db/image.test.ts
@@ -0,0 +1,53 @@
+import {beforeEach, describe} from "mocha";
+import {clearDB} from "../../helpers/database-helper";
+import {assert, expect} from "chai";
+import {UNIVERSAL_PROFILE_1} from "../../helpers/constants";
+import {insertContract} from "../../../bin/db/contract.table";
+import {shouldThrow} from "../../helpers/should-throw";
+import {insertContractMetadata} from "../../../bin/db/contract-metadata.table";
+import {deleteTag, insertTag, queryTags} from "../../../bin/db/tag.table";
+import {deleteImage, insertImage, queryImages, queryImagesByType} from "../../../bin/db/image.table";
+
+export const ImageTests = () => {
+  describe('Table Image', () => {
+
+      beforeEach(async () => {
+          await clearDB();
+          await insertContract(UNIVERSAL_PROFILE_1, null);
+          await insertContractMetadata(UNIVERSAL_PROFILE_1, '', '', '');
+      });
+
+      it ('should be able to insert values', async () => {
+          assert(!await shouldThrow(insertImage(UNIVERSAL_PROFILE_1, '', 0, 0, '')));
+      });
+
+      it ('should be able to query images', async () => {
+          await insertImage(UNIVERSAL_PROFILE_1, 'url', 1, 2, 'profile');
+          const res = await queryImages(UNIVERSAL_PROFILE_1);
+
+          expect(res[0].url).to.be.equal('url');
+          expect(res[0].width).to.be.equal(1);
+          expect(res[0].height).to.be.equal(2);
+          expect(res[0].type).to.be.equal('profile');
+      });
+
+      it ('should be able to query images by type', async () => {
+          await insertImage(UNIVERSAL_PROFILE_1, 'url', 1, 2, 'background');
+          await insertImage(UNIVERSAL_PROFILE_1, 'url2', 2, 1, 'profile');
+          const res = await queryImagesByType(UNIVERSAL_PROFILE_1, 'profile');
+
+          expect(res[0].url).to.be.equal('url2');
+          expect(res[0].width).to.be.equal(2);
+          expect(res[0].height).to.be.equal(1);
+          expect(res[0].type).to.be.equal('profile');
+      });
+
+      it ('should be able to delete an image', async () => {
+          await insertImage(UNIVERSAL_PROFILE_1, 'url', 1, 2, 'background');
+          await deleteImage(UNIVERSAL_PROFILE_1, 'url');
+          const res = await queryImages(UNIVERSAL_PROFILE_1);
+
+          expect(res.length).to.be.equal(0);
+      });
+  });
+}
