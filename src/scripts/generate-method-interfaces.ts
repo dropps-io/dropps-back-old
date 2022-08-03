1a15525350eaebb5e77e2b0cdbf40d113f868b4e
Samuel_V <samuel@dropps.io>
2022-08-03 11:14:46 +0100

    feat: generate method interfaces from abi script

---
+++ src/scripts/generate-method-interfaces.ts
@@ -0,0 +1,47 @@
+import {AbiInput, AbiItem} from "web3-utils";
+import keccak256 from "keccak256";
+import {SolMethod} from "../bin/EthLogs/EthLog.models";
+import {insertMethodInterface} from "../bin/db/method-interface.table";
+import {insertMethodParameter} from "../bin/db/method-parameter.table";
+
+export async function generateAndPersistMethodInterfaces(contractAbis: AbiItem[][]): Promise<SolMethod[]> {
+    const interfaces: SolMethod[] = [];
+
+    contractAbis.forEach(abis => {
+        abis.forEach(abi => {
+            if (abi.name && (abi.type === 'event' || abi.type === 'function')) {
+                const skeleton = generateMethodSkeleton(abi);
+                const methodHash = '0x' + keccak256(skeleton).toString('hex');
+                if (interfaces.filter(x => x.hash === methodHash).length === 0)
+                    interfaces.push({name: abi.name, type: abi.type, hash: methodHash, id: methodHash.slice(0, 10), parameters: abi.inputs as AbiInput[]});
+            }
+        });
+    });
+
+    for (let methodInterface of interfaces) {
+        try {
+            await insertMethodInterface(methodInterface.id, methodInterface.hash, methodInterface.name, methodInterface.type);
+            for (let parameter of methodInterface.parameters) {
+                await insertMethodParameter(methodInterface.id, parameter.name, parameter.type, parameter.indexed);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return interfaces;
+}
+
+function generateMethodSkeleton(abi: AbiItem): string {
+    let skeleton = abi.name + '(';
+
+    if (abi.inputs && abi.inputs.length > 0) {
+        for (let i = 0; i < abi.inputs?.length; i++) {
+            skeleton += abi.inputs[i].type;
+            if (i === abi.inputs.length - 1) skeleton += ')';
+            else skeleton += ',';
+        }
+    } else skeleton += ')';
+
+    return skeleton;
+}
