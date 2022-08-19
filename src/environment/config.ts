a6c531552b8e2071b503da08539ccf9a5ec36519
Samuel_V <samuel@dropps.io>
2022-08-19 20:37:53 +0100

    config: max file size

--- src/environment/config.ts
+++ src/environment/config.ts
@@ -3,3 +3,4 @@ import config from 'config';
 export const ENV = config.get('env');
 export const JWT_VALIDITY_TIME: number = config.get('jwt_validity_time');
 export const LOGGER: boolean = config.get('logger');
+export const MAX_IMAGE_SIZE: boolean = config.get('max_image_size');
