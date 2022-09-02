8d7a6715a31d636d1ff9a0471fff51e1ec64bc16
samuel-videau <samuel.videau@efrei.net>
2022-09-02 14:59:37 +0100

    script: try executing without reverting if error

--- src/scripts/refresh-all-up-metadata.ts
+++ src/scripts/refresh-all-up-metadata.ts
@@ -2,19 +2,20 @@ import {queryContracts} from "../bin/db/contract.table";
 import {extractLSP3Data} from "./blockchain-indexing/data-extraction/contract-metatata/extract-lsp3";
 import {extractLSP7Data} from "./blockchain-indexing/data-extraction/contract-metatata/extract-lsp7";
 import {extractLSP8Data} from "./blockchain-indexing/data-extraction/contract-metatata/extract-lsp8";
+import {tryExecuting} from "../bin/utils/try-executing";
 
 async function refreshAllUpMetadata() {
   const universalProfiles = await queryContracts();
   for (const up of universalProfiles) {
     switch (up.interfaceCode) {
       case 'LSP0':
-        await extractLSP3Data(up.address);
+        await tryExecuting(extractLSP3Data(up.address));
         break;
       case 'LSP7':
-        await extractLSP7Data(up.address);
+        await tryExecuting(extractLSP7Data(up.address));
         break;
       case 'LSP8':
-        await extractLSP8Data(up.address);
+        await tryExecuting(extractLSP8Data(up.address));
         break;
     }
   }
