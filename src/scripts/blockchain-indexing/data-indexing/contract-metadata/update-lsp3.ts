9332359b490205fd9cb597221cbab9706f018577
samuel-videau <samuel.videau@efrei.net>
2022-09-02 14:06:24 +0100

    script: try executing without reverting if error

--- src/scripts/blockchain-indexing/data-indexing/contract-metadata/update-lsp3.ts
+++ src/scripts/blockchain-indexing/data-indexing/contract-metadata/update-lsp3.ts
@@ -7,6 +7,7 @@ import {deleteLink, insertLink, queryLinks} from "../../../../bin/db/link.table"
 import {deleteTag, insertTag, queryTags} from "../../../../bin/db/tag.table";
 import {updateContractDescription, updateContractName} from "../../../../bin/db/contract-metadata.table";
 import {INDEX_DATA} from "../../config";
+import {tryExecuting} from "../../../../bin/utils/try-executing";
 
 export async function updateLSP3Profile(address: string, lsp3: LSP3UniversalProfile) {
   if (!INDEX_DATA) return;
@@ -24,21 +25,21 @@ export async function updateLSP3Profile(address: string, lsp3: LSP3UniversalProf
     const profileImagesToAdd = lsp3.profileImage ? lsp3.profileImage.filter(i => !images.map(x => x.hash).includes(i.hash)) : [];
     const backgroundImagesToAdd = lsp3.backgroundImage ? lsp3.backgroundImage.filter(i => !images.map(x => x.hash).includes(i.hash)) : [];
 
-    for (let image of imagesToDelete) await deleteImage(address, image.url);
-    for (let image of profileImagesToAdd) await insertImage(address, image.url, image.width, image.height, 'profile', image.hash);
-    for (let image of backgroundImagesToAdd) await insertImage(address, image.url, image.width, image.height, 'background', image.hash);
+    for (let image of imagesToDelete) await tryExecuting(deleteImage(address, image.url));
+    for (let image of profileImagesToAdd) await tryExecuting(insertImage(address, image.url, image.width, image.height, 'profile', image.hash));
+    for (let image of backgroundImagesToAdd) await tryExecuting(insertImage(address, image.url, image.width, image.height, 'background', image.hash));
 
     const linksToDelete = links.filter(i => !lsp3.links.map(x => x.url).includes(i.url));
     const linksToAdd = lsp3.links ? lsp3.links.filter(i => !links.map(x => x.url).includes(i.url)) : [];
 
-    for (let link of linksToDelete) await deleteLink(address, link.title, link.url);
-    for (let link of linksToAdd) await insertLink(address, link.title, link.url);
+    for (let link of linksToDelete) await tryExecuting(deleteLink(address, link.title, link.url));
+    for (let link of linksToAdd) await tryExecuting(insertLink(address, link.title, link.url));
 
     const tagsToDelete = tags.filter(i => !lsp3.tags.includes(i));
     const tagsToAdd = lsp3.tags ? lsp3.tags.filter(i => !tags.includes(i)) : [];
 
-    for (let tag of tagsToDelete) await deleteTag(address, tag);
-    for (let tag of tagsToAdd) await insertTag(address, tag);
+    for (let tag of tagsToDelete) await tryExecuting(deleteTag(address, tag));
+    for (let tag of tagsToAdd) await tryExecuting(insertTag(address, tag));
   } catch (e) {
     console.error(e);
   }
--- src/scripts/blockchain-indexing/data-indexing/contract-metadata/update-lsp4.ts
+++ src/scripts/blockchain-indexing/data-indexing/contract-metadata/update-lsp4.ts
@@ -8,6 +8,7 @@ import {Link} from "../../../../models/types/link";
 import {deleteLink, insertLink, queryLinks} from "../../../../bin/db/link.table";
 import {updateContractDescription} from "../../../../bin/db/contract-metadata.table";
 import {INDEX_DATA} from "../../config";
+import {tryExecuting} from "../../../../bin/utils/try-executing";
 
 export async function updateLSP4Metadata(address: string, lsp4: LSP4DigitalAssetMetadata) {
   if (!INDEX_DATA) return;
@@ -24,21 +25,21 @@ export async function updateLSP4Metadata(address: string, lsp4: LSP4DigitalAsset
     const imagesToAdd = lsp4.images ? lsp4.images.filter(i => !images.map(x => x.hash).includes(i.hash)) : [];
     const iconsToAdd = lsp4.icon ? lsp4.icon.filter(i => !images.map(x => x.hash).includes(i.hash)) : [];
 
-    for (let image of imagesToDelete) await deleteImage(address, image.url);
-    for (let image of imagesToAdd) await insertImage(address, image.url, image.width, image.height, '', image.hash);
-    for (let image of iconsToAdd) await insertImage(address, image.url, image.width, image.height, 'icon', image.hash);
+    for (let image of imagesToDelete) await tryExecuting(deleteImage(address, image.url));
+    for (let image of imagesToAdd) await tryExecuting(insertImage(address, image.url, image.width, image.height, '', image.hash));
+    for (let image of iconsToAdd) await tryExecuting(insertImage(address, image.url, image.width, image.height, 'icon', image.hash));
 
     const assetsToDelete = assets.filter(i => !lsp4.assets.map(x => x.hash).includes(i.hash));
     const assetsToAdd = lsp4.assets ? lsp4.assets.filter(i => !assets.map(x => x.hash).includes(i.hash)) : [];
 
-    for (let asset of assetsToDelete) await deleteAsset(address, asset.url);
-    for (let asset of assetsToAdd) await insertAsset(address, asset.url, asset.fileType, asset.hash);
+    for (let asset of assetsToDelete) await tryExecuting(deleteAsset(address, asset.url));
+    for (let asset of assetsToAdd) await tryExecuting(insertAsset(address, asset.url, asset.fileType, asset.hash));
 
     const linksToDelete = links.filter(i => !lsp4.links.map(x => x.url).includes(i.url));
     const linksToAdd = lsp4.links ? lsp4.links.filter(i => !links.map(x => x.url).includes(i.url)) : [];
 
-    for (let link of linksToDelete) await deleteLink(address, link.title, link.url);
-    for (let link of linksToAdd) await insertLink(address, link.title, link.url);
+    for (let link of linksToDelete) await tryExecuting(deleteLink(address, link.title, link.url));
+    for (let link of linksToAdd) await tryExecuting(insertLink(address, link.title, link.url));
   } catch (e) {
     console.error(e);
   }
