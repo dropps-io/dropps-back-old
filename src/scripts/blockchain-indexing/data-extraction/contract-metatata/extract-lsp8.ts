e37ecde6c4e063330e242428245f34c3b5de6836
samuel-videau <samuel.videau@efrei.net>
2022-09-02 15:01:40 +0100

    script: await indexing

--- src/scripts/blockchain-indexing/data-extraction/contract-metatata/extract-lsp8.ts
+++ src/scripts/blockchain-indexing/data-extraction/contract-metatata/extract-lsp8.ts
@@ -4,5 +4,5 @@ import {indexLSP4Data} from "../../data-indexing/contract-metadata/index-lsp4";
 export async function extractLSP8Data(address: string): Promise<void> {
   const lsp4 = await extractLSP4Data(address)
 
-  indexLSP4Data(address, lsp4, true, '0');
+  await indexLSP4Data(address, lsp4, true, '0');
 }
