e3816931243290dfbc5998657e4776add358e581
samuel-videau <samuel.videau@efrei.net>
2022-09-21 13:49:32 +0100

    chore: scripts to run different envs

--- src/environment/endpoints.ts
+++ src/environment/endpoints.ts
@@ -6,6 +6,7 @@ export const NODE_ENV = (process.env.NODE_ENV || 'development') as 'test' | 'dev
 
 if (NODE_ENV === 'test') config({ path: path.resolve(process.cwd(), '.env.test') });
 if (NODE_ENV === 'production') config({ path: path.resolve(process.cwd(), '.env.prod') });
+if (NODE_ENV === 'staging') config({ path: path.resolve(process.cwd(), '.env.staging') });
 
 config();
 
