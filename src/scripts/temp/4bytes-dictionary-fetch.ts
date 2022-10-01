b4610ed96d66896122ec20d510faf1296ab3f898
samuel-videau <samuel.videau@efrei.net>
2022-10-01 18:26:14 +0100

    script recursive in case of error

--- src/scripts/temp/4bytes-dictionary-fetch.ts
+++ src/scripts/temp/4bytes-dictionary-fetch.ts
@@ -3,30 +3,36 @@ import {textMethodSignatureExtraction} from "../../bin/utils/text-signature-extr
 import {insertMethodInterface} from "../../bin/db/method-interface.table";
 import {insertMethodParameter} from "../../bin/db/method-parameter.table";
 import {executeQuery} from "../../bin/db/database";
+import {sleep} from "../blockchain-indexing/utils/sleep";
 
-export async function fourBytesDictionaryFetch() {
-  let next, n = 0;
-  do {
-    const methodInterfaces: any = await axios.get(next ? next : 'https://www.4byte.directory/api/v1/signatures/?ordering=created_at&page=0');
-    next = methodInterfaces.data.next;
-    for (const result of (methodInterfaces.data.results as any[])) {
-      try {
-        await executeQuery('DELETE FROM method_parameter WHERE "methodId" = $1', [result.hex_signature])
-        const extractedMethod = textMethodSignatureExtraction(result.text_signature);
-        await insertMethodInterface(result.hex_signature, '', extractedMethod.name, 'function');
-        let n = 0;
-        for (const param of extractedMethod.params) {
-          await insertMethodParameter(result.hex_signature, '', param, n);
-          n++;
+export async function fourBytesDictionaryFetch(_n: number, _next?: string) {
+  let next= _next, n = _n;
+  try {
+    do {
+      const methodInterfaces: any = await axios.get(next ? next : 'https://www.4byte.directory/api/v1/signatures/?ordering=created_at&page=0');
+      next = methodInterfaces.data.next;
+      for (const result of (methodInterfaces.data.results as any[])) {
+        try {
+          await executeQuery('DELETE FROM method_parameter WHERE "methodId" = $1', [result.hex_signature])
+          const extractedMethod = textMethodSignatureExtraction(result.text_signature);
+          await insertMethodInterface(result.hex_signature, '', extractedMethod.name, 'function');
+          let n = 0;
+          for (const param of extractedMethod.params) {
+            await insertMethodParameter(result.hex_signature, '', param, n);
+            n++;
+          }
+        } catch (e) {
+          console.error(e);
         }
-      } catch (e) {
-        console.error(e);
       }
-    }
-    console.log('Page ' + n + ' extracted');
-    console.log(next)
-    n++;
-  } while (next);
+      console.log('Page ' + n + ' extracted');
+      console.log(next)
+      n++;
+    } while (next);
+  } catch (e) {
+    await sleep(5000);
+    fourBytesDictionaryFetch(n, next);
+  }
 }
 
-fourBytesDictionaryFetch();
+fourBytesDictionaryFetch(0);
