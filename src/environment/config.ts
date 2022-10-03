89522a710e7a2b289647a136c0c909d1c854df21
samuel-videau <samuel.videau@efrei.net>
2022-10-03 18:09:13 +0100

    config: api_url

--- src/environment/config.ts
+++ src/environment/config.ts
@@ -7,7 +7,10 @@ export const MAX_IMAGE_SIZE: boolean = config.get('max_image_size');
 export const POST_VALIDATOR_ADDRESS: string[] = config.get('post_validator_address');
 export const HTTP_RPC: string = config.get('http_rpc');
 export const PORT: string = config.get('port');
+export const API_URL: string = config.get('api_url');
 
 export const MAX_OFFCHAIN_REGISTRY_CHANGES = 30;
+export const POSTS_PER_LOAD = 30;
+export const PROFILES_PER_LOAD = 50;
 export const IPFS_GATEWAY = 'https://2eff.lukso.dev/ipfs/';
 export const ARWEAVE_GATEWAY = 'https://arweave.net/';
