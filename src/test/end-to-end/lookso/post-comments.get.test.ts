cb9128f3af9570797e052ade35ba58c6b45d6d1b
samuel-videau <samuel.videau@efrei.net>
2022-10-09 14:07:15 +0100

    test: get comments with pagination

--- src/test/end-to-end/lookso/post-comments.get.test.ts
+++ src/test/end-to-end/lookso/post-comments.get.test.ts
@@ -11,13 +11,15 @@ import {FeedPost} from "../../../models/types/feed-post";
 import {
   HACKER_MAN_UP, POST_HASH, POST_HASH2, POST_HASH3, POST_HASH4, POST_HASH5, SERIOUS_MAN_UP, UNIVERSAL_PROFILE_1, UNIVERSAL_PROFILE_2
 } from "../../helpers/constants";
+import {API_URL, COMMENTS_PER_LOAD} from "../../../environment/config";
+import {generateRandomKeccakHash} from "../../helpers/generate-mocks";
 
 export const PostCommentsGETTests = () => {
 
   describe('GET lookso/post/:hash/comments', () => {
 
     let res: LightMyRequestResponse;
-    let feed: FeedPost[];
+    let payload: {count: number, next: string | null, previous: string | null, results: FeedPost[]};
 
     beforeEach(async () => {
       await clearDB();
@@ -34,7 +36,7 @@ export const PostCommentsGETTests = () => {
       await insertLike(HACKER_MAN_UP, POST_HASH2);
       await insertLike(HACKER_MAN_UP, POST_HASH4);
       res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments`});
-      feed = JSON.parse(res.payload);
+      payload = JSON.parse(res.payload);
     });
 
     it('should return 200', async () => {
@@ -42,35 +44,23 @@ export const PostCommentsGETTests = () => {
     });
 
     it('should return the right amount of comments', async () => {
-      expect(feed.length).to.equal(4);
+      expect(payload.results.length).to.equal(4);
     });
 
     it('should return posts in chronological order', async () => {
-      expect(feed[0].hash).to.equal(POST_HASH5);
-      expect(feed[1].hash).to.equal(POST_HASH4);
-      expect(feed[2].hash).to.equal(POST_HASH3);
-      expect(feed[3].hash).to.equal(POST_HASH2);
+      expect(payload.results[3].hash).to.equal(POST_HASH5);
+      expect(payload.results[2].hash).to.equal(POST_HASH4);
+      expect(payload.results[1].hash).to.equal(POST_HASH3);
+      expect(payload.results[0].hash).to.equal(POST_HASH2);
     });
 
     it('should show if a post is liked or not if viewOf', async () => {
       res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments?viewOf=${HACKER_MAN_UP}`});
-      feed = JSON.parse(res.payload);
-      expect(feed.filter(p => p.hash === POST_HASH2)[0].isLiked).to.equal(true);
-      expect(feed.filter(p => p.hash === POST_HASH3)[0].isLiked).to.equal(false);
-      expect(feed.filter(p => p.hash === POST_HASH4)[0].isLiked).to.equal(true);
-      expect(feed.filter(p => p.hash === POST_HASH5)[0].isLiked).to.equal(false);
-    });
-
-    it('should work with limit', async () => {
-      res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments?limit=1`});
-      feed = JSON.parse(res.payload);
-      expect(feed.length).to.equal(1);
-    });
-
-    it('should work with offset', async () => {
-      res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments?offset=1`});
-      feed = JSON.parse(res.payload);
-      expect(feed.length).to.equal(3);
+      payload = JSON.parse(res.payload);
+      expect(payload.results.filter(p => p.hash === POST_HASH2)[0].isLiked).to.equal(true);
+      expect(payload.results.filter(p => p.hash === POST_HASH3)[0].isLiked).to.equal(false);
+      expect(payload.results.filter(p => p.hash === POST_HASH4)[0].isLiked).to.equal(true);
+      expect(payload.results.filter(p => p.hash === POST_HASH5)[0].isLiked).to.equal(false);
     });
 
     it('should return 400 if invalid hash', async () => {
@@ -82,5 +72,58 @@ export const PostCommentsGETTests = () => {
       res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments?viewOf=${HACKER_MAN_UP}q`});
       expect(res.statusCode).to.equal(400);
     });
+
+    describe ('With pagination', () => {
+      beforeEach( async () => {
+        for (let i = 0 ; i < COMMENTS_PER_LOAD * 2 - 4 ; i++) {
+          await insertPost(generateRandomKeccakHash(), SERIOUS_MAN_UP, new Date('2022-09-27T12:03:32.089Z'), 'test1', '', POST_HASH, null, null);
+        }
+      });
+
+      it ('should return the previous page when not in the query', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(`${API_URL}/lookso/post/${POST_HASH}/comments?page=0`);
+      });
+
+      it ('should return the right amount of posts', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments`});
+        payload = JSON.parse(res.payload);
+        expect(payload.results.length).to.be.equal(COMMENTS_PER_LOAD);
+      });
+
+      it ('should return the next page', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments?page=0`});
+        payload = JSON.parse(res.payload);
+        expect(payload.previous).to.be.null;
+        expect(payload.next).to.equal(`${API_URL}/lookso/post/${POST_HASH}/comments?page=1`);
+      });
+
+      it ('should return the next page with query params', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments?viewOf=${SERIOUS_MAN_UP}&page=0`});
+        payload = JSON.parse(res.payload);
+        expect(payload.previous).to.be.null;
+        expect(payload.next).to.equal(`${API_URL}/lookso/post/${POST_HASH}/comments?viewOf=${SERIOUS_MAN_UP}&page=1`);
+      });
+
+      it ('should return the previous page with query params', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments?viewOf=${SERIOUS_MAN_UP}`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(`${API_URL}/lookso/post/${POST_HASH}/comments?viewOf=${SERIOUS_MAN_UP}&page=0`);
+      });
+
+      it ('should return only one post on the last page if 61 posts in the feed', async () => {
+        await insertPost(generateRandomKeccakHash(), SERIOUS_MAN_UP, new Date('2022-09-27T12:03:32.089Z'), 'test1', '', POST_HASH, null, null);
+        res = await fastify.inject({method: 'GET', url: `/lookso/post/${POST_HASH}/comments`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(`${API_URL}/lookso/post/${POST_HASH}/comments?page=1`);
+        expect(payload.count).to.equal((COMMENTS_PER_LOAD  * 2 + 1));
+        expect(payload.results.length).to.equal(1);
+      });
+    });
+
   });
 }
