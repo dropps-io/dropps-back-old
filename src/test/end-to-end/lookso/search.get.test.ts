1a6c9f21289b76c58fa62b488f547fb8a844d0a5
samuel-videau <samuel.videau@efrei.net>
2022-10-09 11:59:01 +0100

    test: search pagination

--- src/test/end-to-end/lookso/search.get.test.ts
+++ src/test/end-to-end/lookso/search.get.test.ts
@@ -7,9 +7,14 @@ import {assert, expect} from "chai";
 import {HACKER_MAN_UP, HACKER_MAN_UP_CLOSE_ADDRESS, SERIOUS_MAN_UP} from "../../helpers/constants";
 import {insertContractMetadata} from "../../../bin/db/contract-metadata.table";
 import {insertImage} from "../../../bin/db/image.table";
+import {FeedPost} from "../../../models/types/feed-post";
+import {API_URL, PROFILES_PER_SEARCH} from "../../../environment/config";
+import {generateRandomAddress} from "../../helpers/generate-mocks";
 
 export const SearchGETTests = () => {
   describe('GET lookso/search/:input', () => {
+
+    let payload: {count: number, next: string | null, previous: string | null, results: FeedPost[]};
 
     beforeEach(async () => {
       await clearDB();
@@ -27,12 +32,12 @@ export const SearchGETTests = () => {
 
     it('should return the right profile with name', async () => {
       let res = await fastify.inject({method: 'GET', url: `/lookso/search/HackerMan`});
-      let profiles: any[] = JSON.parse(res.payload);
+      let profiles: any[] = JSON.parse(res.payload).results;
       expect(profiles.length).to.equal(1);
       expect(profiles[0].address).to.equal(HACKER_MAN_UP);
 
       res = await fastify.inject({method: 'GET', url: `/lookso/search/SeriousMan`});
-      profiles = JSON.parse(res.payload);
+      profiles = JSON.parse(res.payload).results;
       expect(profiles.length).to.equal(1);
       expect(profiles[0].address).to.equal(SERIOUS_MAN_UP);
     });
@@ -40,29 +45,32 @@ export const SearchGETTests = () => {
     it('should return name and image', async () => {
       await insertImage(HACKER_MAN_UP, 'url', 300, 300, 'profile', '0x00');
       const res = await fastify.inject({method: 'GET', url: `/lookso/search/hackerman`});
-      const profiles: any[] = JSON.parse(res.payload);
+      const profiles: any[] = JSON.parse(res.payload).results;
       expect(profiles[0].image).to.equal('url');
       expect(profiles[0].name).to.equal('HackerMan');
     });
 
     it('should not be case sensitive', async () => {
       const res = await fastify.inject({method: 'GET', url: `/lookso/search/hackerman`});
-      const profiles: any[] = JSON.parse(res.payload);
+      const profiles: any[] = JSON.parse(res.payload).results;
       expect(profiles.length).to.equal(1);
       expect(profiles[0].address).to.equal(HACKER_MAN_UP);
     });
 
     it('should find all profiles containing an input', async () => {
       const res = await fastify.inject({method: 'GET', url: `/lookso/search/man`});
-      const profiles: any[] = JSON.parse(res.payload);
-      expect(profiles.length).to.equal(2);
+      payload = JSON.parse(res.payload);
+      expect(payload.results.length).to.equal(2);
+      expect(payload.count).to.equal('2');
+      expect(payload.next).to.be.null;
+      expect(payload.previous).to.be.null;
     });
 
     it('should find all profiles containing a partial address', async () => {
       await insertContract(HACKER_MAN_UP_CLOSE_ADDRESS, 'LSP0');
       await insertContractMetadata(HACKER_MAN_UP_CLOSE_ADDRESS, 'HackerManCloseAddress', '', '', false, '');
       const res = await fastify.inject({method: 'GET', url: `/lookso/search/${HACKER_MAN_UP.slice(0, 6)}`});
-      const profiles: any[] = JSON.parse(res.payload);
+      const profiles: any[] = JSON.parse(res.payload).results;
       expect(profiles.length).to.equal(2);
       assert(profiles.map(p => p.address).includes(HACKER_MAN_UP_CLOSE_ADDRESS));
       assert(profiles.map(p => p.address).includes(HACKER_MAN_UP));
@@ -70,21 +78,51 @@ export const SearchGETTests = () => {
 
     it('should find profile with address', async () => {
       const res = await fastify.inject({method: 'GET', url: `/lookso/search/${SERIOUS_MAN_UP}`});
-      const profiles: any[] = JSON.parse(res.payload);
+      const profiles: any[] = JSON.parse(res.payload).results;
       expect(profiles.length).to.equal(1);
       expect(profiles[0].address).to.equal(SERIOUS_MAN_UP);
     });
 
-    it('should work with limit', async () => {
-      const res = await fastify.inject({method: 'GET', url: `/lookso/search/man?limit=1`});
-      const profiles: any[] = JSON.parse(res.payload);
-      expect(profiles.length).to.equal(1);
-    });
+    describe ('With pagination', () => {
+      beforeEach(async () => {
+        for (let i = 0; i < PROFILES_PER_SEARCH * 2 - 2; i++) {
+          const address: string = generateRandomAddress();
+          await insertContract(address, 'LSP0');
+          await insertContractMetadata(address, 'man', '', '', false, '');
+        }
+      });
 
-    it('should work with offset', async () => {
-      const res = await fastify.inject({method: 'GET', url: `/lookso/search/man?offset=1`});
-      const profiles: any[] = JSON.parse(res.payload);
-      expect(profiles.length).to.equal(1);
+      it ('should return the next page when not in the query', async () => {
+        const res = await fastify.inject({method: 'GET', url: `/lookso/search/man`});
+        payload = JSON.parse(res.payload);
+        expect(payload.previous).to.be.null;
+        expect(payload.next).to.equal(API_URL + '/lookso/search/man?page=1');
+      });
+
+      it ('should return the right amount of posts', async () => {
+        const res = await fastify.inject({method: 'GET', url: `/lookso/search/man`});
+        payload = JSON.parse(res.payload);
+        expect(payload.results.length).to.be.equal(PROFILES_PER_SEARCH);
+      });
+
+      it ('should return the next page', async () => {
+        const res = await fastify.inject({method: 'GET', url: `/lookso/search/man?page=1`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(API_URL + '/lookso/search/man?page=0');
+      });
+
+      it ('should return only one post on the last page if 61 posts in the feed', async () => {
+        const address: string = generateRandomAddress();
+        await insertContract(address, 'LSP0');
+        await insertContractMetadata(address, 'man', '', '', false, '');
+        const res = await fastify.inject({method: 'GET', url: `/lookso/search/man?page=2`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(API_URL + '/lookso/search/man?page=1');
+        expect(payload.count).to.equal((PROFILES_PER_SEARCH  * 2 + 1).toString());
+        expect(payload.results.length).to.equal(1);
+      });
     });
   });
 }
--- src/test/helpers/generate-mocks.ts
+++ src/test/helpers/generate-mocks.ts
@@ -3,3 +3,7 @@ import keccak256 from "keccak256";
 export function generateRandomKeccakHash(): string {
   return '0x' + keccak256(Math.random().toString()).toString('hex');
 }
+
+export function generateRandomAddress(): string {
+  return '0x' + keccak256(Math.random().toString()).toString('hex').slice(0, 40);
+}
