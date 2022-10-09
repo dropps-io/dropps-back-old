c7cee41b38968abb2bd0504bce3ce8c40f7b6600
samuel-videau <samuel.videau@efrei.net>
2022-10-09 13:42:59 +0100

    test: profile feed with pagination

--- src/test/end-to-end/lookso/profile-feed.get.test.ts
+++ src/test/end-to-end/lookso/profile-feed.get.test.ts
@@ -14,13 +14,15 @@ import {
   HACKER_MAN_UP, POST_HASH, POST_HASH2, POST_HASH3, POST_HASH4, POST_HASH5, POST_HASH6, SERIOUS_MAN_UP, UNIVERSAL_PROFILE_1, UNIVERSAL_PROFILE_2
 } from "../../helpers/constants";
 import {insertFollow} from "../../../bin/db/follow.table";
+import {API_URL, POSTS_PER_LOAD} from "../../../environment/config";
+import {generateRandomKeccakHash} from "../../helpers/generate-mocks";
 
 export const ProfileFeedGETTests = () => {
 
   describe('GET lookso/profile/:address/feed', () => {
 
     let res: LightMyRequestResponse;
-    let feed: FeedPost[];
+    let payload: {count: number, next: string | null, previous: string | null, results: FeedPost[]};
 
     beforeEach(async () => {
       await clearDB();
@@ -39,7 +41,7 @@ export const ProfileFeedGETTests = () => {
       await insertFollow(HACKER_MAN_UP, SERIOUS_MAN_UP);
       await insertFollow(HACKER_MAN_UP, UNIVERSAL_PROFILE_1);
       res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed`});
-      feed = JSON.parse(res.payload);
+      payload = JSON.parse(res.payload);
     });
 
     it('should return 200', async () => {
@@ -47,29 +49,17 @@ export const ProfileFeedGETTests = () => {
     });
 
     it('should return 2 feed posts', async () => {
-      expect(feed.length).to.equal(2);
+      expect(payload.results.length).to.equal(2);
     });
 
     it('should return posts in chronological order', async () => {
-      expect(feed[0].hash).to.equal(POST_HASH4);
-      expect(feed[1].hash).to.equal(POST_HASH3);
+      expect(payload.results[1].hash).to.equal(POST_HASH4);
+      expect(payload.results[0].hash).to.equal(POST_HASH3);
     });
 
     it('should show if a post is liked or not', async () => {
-      expect(feed.filter(p => p.hash === POST_HASH3)[0].isLiked).to.equal(false);
-      expect(feed.filter(p => p.hash === POST_HASH4)[0].isLiked).to.equal(true);
-    });
-
-    it('should work with limit', async () => {
-      res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed?limit=1`});
-      feed = JSON.parse(res.payload);
-      expect(feed.length).to.equal(1);
-    });
-
-    it('should work with offset', async () => {
-      res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed?offset=1`});
-      feed = JSON.parse(res.payload);
-      expect(feed.length).to.equal(1);
+      expect(payload.results.filter(p => p.hash === POST_HASH3)[0].isLiked).to.equal(false);
+      expect(payload.results.filter(p => p.hash === POST_HASH4)[0].isLiked).to.equal(true);
     });
 
     it('should work with post filter', async () => {
@@ -77,9 +67,9 @@ export const ProfileFeedGETTests = () => {
       const id = await insertEvent(UNIVERSAL_PROFILE_1, POST_HASH, '', 0, '', '');
       await insertPost(POST_HASH6, UNIVERSAL_PROFILE_1, new Date('2022-09-27T12:03:36.089Z'), 'test1', '', null, null, id);
       res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed?postType=post`});
-      feed = JSON.parse(res.payload);
-      expect(feed.length).to.equal(2);
-      assert(feed.every(p => p.type === 'post'));
+      payload = JSON.parse(res.payload);
+      expect(payload.results.length).to.equal(2);
+      assert(payload.results.every(p => p.type === 'post'));
     });
 
     it('should work with event filter', async () => {
@@ -87,14 +77,67 @@ export const ProfileFeedGETTests = () => {
       const id = await insertEvent(UNIVERSAL_PROFILE_1, POST_HASH, '', 0, '', '');
       await insertPost(POST_HASH6, UNIVERSAL_PROFILE_1, new Date('2022-09-27T12:03:36.089Z'), 'test1', '', null, null, id);
       res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed?postType=event`});
-      feed = JSON.parse(res.payload);
-      expect(feed.length).to.equal(1);
-      assert(feed.every(p => p.type === 'event'));
+      payload = JSON.parse(res.payload);
+      expect(payload.results.length).to.equal(1);
+      assert(payload.results.every(p => p.type === 'event'));
     });
 
     it('should return 400 if invalid address', async () => {
       res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}q/feed`});
       expect(res.statusCode).to.equal(400);
     });
+
+    describe ('With pagination', () => {
+      beforeEach( async () => {
+        for (let i = 0 ; i < POSTS_PER_LOAD * 2 - 2 ; i++) {
+          await insertPost(generateRandomKeccakHash(), SERIOUS_MAN_UP, new Date('2022-09-27T12:03:32.089Z'), 'test1', '', null, null, null);
+        }
+      });
+
+      it ('should return the previous page when not in the query', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(`${API_URL}/lookso/profile/${HACKER_MAN_UP}/feed?page=0`);
+      });
+
+      it ('should return the right amount of posts', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed`});
+        payload = JSON.parse(res.payload);
+        expect(payload.results.length).to.be.equal(POSTS_PER_LOAD);
+      });
+
+      it ('should return the next page', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed?page=0`});
+        payload = JSON.parse(res.payload);
+        expect(payload.previous).to.be.null;
+        expect(payload.next).to.equal(`${API_URL}/lookso/profile/${HACKER_MAN_UP}/feed?page=1`);
+      });
+
+      it ('should return the next page with query params', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed?postType=post&page=0`});
+        payload = JSON.parse(res.payload);
+        expect(payload.previous).to.be.null;
+        expect(payload.next).to.equal(`${API_URL}/lookso/profile/${HACKER_MAN_UP}/feed?postType=post&page=1`);
+      });
+
+      it ('should return the previous page with query params', async () => {
+        res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed?postType=post&page=1`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(`${API_URL}/lookso/profile/${HACKER_MAN_UP}/feed?postType=post&page=0`);
+      });
+
+      it ('should return only one post on the last page if 61 posts in the feed', async () => {
+        await insertPost(generateRandomKeccakHash(), SERIOUS_MAN_UP, new Date('2022-09-27T12:03:32.089Z'), 'test1', '', null, null, null);
+        res = await fastify.inject({method: 'GET', url: `/lookso/profile/${HACKER_MAN_UP}/feed`});
+        payload = JSON.parse(res.payload);
+        expect(payload.next).to.be.null;
+        expect(payload.previous).to.equal(`${API_URL}/lookso/profile/${HACKER_MAN_UP}/feed?page=1`);
+        expect(payload.count).to.equal((POSTS_PER_LOAD  * 2 + 1).toString());
+        expect(payload.results.length).to.equal(1);
+      });
+    });
+
   });
 }
