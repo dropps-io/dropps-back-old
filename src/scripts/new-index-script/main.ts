13e8bd99220cd3be2f24181a31c91a5260123b75
samuel-videau <samuel.videau@efrei.net>
2022-10-13 23:20:34 +0100

    main file index script

--- src/scripts/new-index-script/main.ts
+++ src/scripts/new-index-script/main.ts
@@ -1,9 +1,7 @@
-import {indexL16, setValueOnConfig} from "./index-l16";
+import {indexL16} from "./index-l16";
 
 function main() {
-  setValueOnConfig('latest_indexed_block', '60000').then(() => {
-    indexL16().then();
-  });
+  indexL16().then();
 }
 
 main();
