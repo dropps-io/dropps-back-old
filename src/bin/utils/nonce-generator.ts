bdf90773c779a22567212e7c579f3142402ae900
samuel-videau <samuel.videau@efrei.net>
2022-10-28 15:07:06 +0100

    fix: generate nonce with always 8 numbers and letters

--- src/bin/utils/nonce-generator.ts
+++ src/bin/utils/nonce-generator.ts
@@ -1,3 +1,8 @@
 export function generateRandomNonce(): string {
-	return (Math.floor(Math.random() * 100000000)).toString();
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 8; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return result;
 }
