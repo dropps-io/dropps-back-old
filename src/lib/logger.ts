4770b8ba35ed2dd7774ba8395a457454718a27f2
Destin Martin <destin@destin.dev>
2023-03-02 18:27:14 -0600

    Added winston logger as a dependency

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,9 +1,17 @@
+import winston from 'winston';
 import { LOGGER } from '../environment/config';
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+
+  transports: [new winston.transports.Console()],
+});
+
 export function logMessage(message: any) {
-  if (LOGGER) console.log(message);
+  if (LOGGER) logger.info(message);
 }
 
 export function logError(message: any) {
-  if (LOGGER) console.error(message);
+  if (LOGGER) logger.error(message);
 }
