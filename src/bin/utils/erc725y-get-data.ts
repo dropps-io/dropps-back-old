78568e9ae8b5ae6c121bf36db7f8e1a972c4800a
samuel-videau <samuel.videau@efrei.net>
2023-07-06 14:40:46 +0700

    feat: fetch metadata from tokens with baseURI

--- src/bin/utils/erc725y-get-data.ts
+++ src/bin/utils/erc725y-get-data.ts
@@ -1,11 +1,28 @@
 import { ethers } from 'ethers';
-import ERC725Y_artifact from '@lukso/lsp-smart-contracts/artifacts/LSP0ERC725Account.json';
 import {HTTP_RPC} from '../../environment/config';
 
 const provider = new ethers.JsonRpcProvider(HTTP_RPC);
 
 export const erc725yGetData = async (address: string, dataKey: string): Promise<string | null> => {
-	const contract = new ethers.Contract(address, ERC725Y_artifact.abi, provider);
+	const contract = new ethers.Contract(address,     [{
+		'inputs': [
+			{
+				'internalType': 'bytes32',
+				'name': 'dataKey',
+				'type': 'bytes32'
+			}
+		],
+		'name': 'getData',
+		'outputs': [
+			{
+				'internalType': 'bytes',
+				'name': 'dataValue',
+				'type': 'bytes'
+			}
+		],
+		'stateMutability': 'view',
+		'type': 'function'
+	}], provider);
 	const response = await contract.getData(dataKey);
 	if (!response || response === '0x') return null;
 	else return response;
